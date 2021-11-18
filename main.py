a987350ce696d05a7406441dbd2c217c9b2e1f3c
Nicolò Albergoni <albergoni.nicolo@gmail.com>
2021-11-18 11:17:03 +0100

    Add code to automatically download model weights

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import tensorflow as tf
+from tensorflow.keras.utils import get_file
+
 from model import Deeplabv3
 
+WEIGHTS_PATH_X = "https://github.com/bonlime/keras-deeplab-v3-plus/releases/download/1.1/deeplabv3_xception_tf_dim_ordering_tf_kernels.h5"
+weights_path = get_file('deeplabv3_xception_tf_dim_ordering_tf_kernels.h5',
+                        WEIGHTS_PATH_X,
+                        cache_dir="model",
+                        cache_subdir=""
+                        )
+
 model = Deeplabv3()
-
-# model.summary()
-
+model.load_weights(weights_path, by_name=True)
 
 tf.keras.utils.plot_model(
     model, to_file='./model.png', show_shapes=True, show_dtype=False,
